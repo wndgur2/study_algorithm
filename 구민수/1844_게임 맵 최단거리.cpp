09dd9a59d2b5c8d53fdc5c7fb991ed4c4a983d67
publicminsu@naver.com <publicminsu@naver.com>
2022-12-30 17:05:23 +0900

    221230

---
+++ 구민수/1844_게임 맵 최단거리.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <queue>
+using namespace std;
+int dx[] = {1, -1, 0, 0}, dy[] = {0, 0, 1, -1};
+int solution(vector<vector<int>> maps)
+{
+    queue<pair<int, int>> bfs;
+    bfs.push({0, 0});
+    while (!bfs.empty())
+    {
+        auto cur = bfs.front();
+        bfs.pop();
+        for (int i = 0; i < 4; ++i)
+        {
+            pair<int, int> next = {cur.first + dy[i], cur.second + dx[i]};
+            if (next.first < 0 | next.second < 0 | next.first >= maps.size() | next.second >= maps[next.first].size())
+                continue;
+            if (!maps[next.first][next.second] || maps[next.first][next.second] > 1)
+                continue;
+            if (next.first == maps.size() - 1 && next.second == maps[next.first].size() - 1)
+            {
+                return maps[cur.first][cur.second] + 1;
+                ;
+            }
+            else
+            {
+                maps[next.first][next.second] = maps[cur.first][cur.second] + 1;
+                bfs.push(next);
+            }
+        }
+    }
+    return -1;
+}
