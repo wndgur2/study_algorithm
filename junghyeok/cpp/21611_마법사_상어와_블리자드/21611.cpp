cedff60cfc58710ee66fc7d153a5028e2518e5a1
leejunghyeok <wndgur76@naver.com>
2023-07-07 17:48:01 +0900

    마법상어

--- junghyeok/cpp/21611_마법사_상어와_블리자드/21611.cpp
+++ junghyeok/cpp/21611_마법사_상어와_블리자드/21611.cpp
@@ -1,52 +1,128 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-/*
-    블리자드(방향, 거리)를 써서 구슬을 파괴.
-    구슬이 또르르 굴러와 빈자리를 채움.
-    연속된 구슬이 4개 이상이면 폭발.
-    다시 굴러옴.
-    폭발하는 구슬이 없을 때까지 반복.
-    각 연속된 구슬 그룹은 2개의 구슬(연속된 구슬의 개수, 그 번호)로 변화.
+void 구슬당기기(vector<int> &marbles_){
+    int n = marbles_.size();
+    marbles_.erase(remove(marbles_.begin()+1, marbles_.end(), 0), marbles_.end());
+    while(marbles_.size() < n)
+        marbles_.push_back(0);
+    return;
+}
 
-    위 과정을 M번 반복.
+int 구슬폭발(vector<int> &marbles_){
+    int found_new_i = 0, cur_n = 0, k, l, result = 0;
+    bool is_blown_up = true;
+    while(is_blown_up){
+        is_blown_up = false;
+        for(k=1; k<marbles_.size(); ++k){ // 0번은 마법사 상어니까 1번부터
+            if(marbles_[k] != cur_n){
+                if(k - found_new_i > 3){ // 폭발 발생
+                    result += cur_n * (k-found_new_i);
+                    is_blown_up = true;
+                    for(l=found_new_i; l<k; ++l)
+                        marbles_[l] = 0;
+                }
+                cur_n = marbles_[k];
+                found_new_i = k;
+            }
+            if(marbles_[k] == 0) break;
+        }
+        구슬당기기(marbles_);
+    }
+    return result;
+}
 
-    1×(폭발한 1번 구슬의 개수) + 2×(폭발한 2번 구슬의 개수) + 3×(폭발한 3번 구슬의 개수) 출력
+vector<int> 구슬변화(vector<int> &marbles_){
+    vector<int> new_marbles = {0};
+    int max_size = marbles_.size();
+    int group_size = 0, cur_n = marbles_[1], k, l;
+    for(k=1; k<max_size; ++k){ // 0번은 마법사 상어니까 1번부터
+        if(marbles_[k] != cur_n){ // 그룹 생성됨
+            // new_marbles.push_back(group_size);
+            // new_marbles.push_back(cur_n);
+            if(new_marbles.size() < max_size) new_marbles.push_back(group_size);
+            else break;
+            if(new_marbles.size() < max_size) new_marbles.push_back(cur_n);
+            else break;
+            
+            if(marbles_[k] == 0) break;
+            
+            cur_n = marbles_[k];
+            group_size = 0;
+        }
 
-    -----------------------
+        ++group_size;
+    }
+    while(new_marbles.size() < max_size)
+        new_marbles.push_back(0);
 
-    블리자드 대상 인덱스 구하는 법
-
-    달팽이 껍질 모양의 테이블
-    안쪽 칸에서 바깥쪽 칸으로 가기 위한 인덱스 차이는,
-    껍질이 커질 수록 커진다.
-
-    그 증가량은, 껍질이 코너를 돌 때 1씩 2번 증가한다.
-
-    7 7 8 9 9 10 11 11 11 12 13 13 13 14 15 15 15 15 16 16 16 16 17 17 17 17 17
-
-    새로운 인덱스 차이를 구했으면, 해당 껍데기의 길이동안 유지된다. (꺾기 전까지 유지된다.)
-
-    껍데기의 길이는 (북, 서), (남, 동)이 동일하다.
-
-    북으로 3 쐈으면 파괴되는 인덱스는
-    7 22 45
-    인덱스 증가값은 각 7, 15, 23
-
-    7: 초기값
-    15: 7 + 2 + 2 + 2 + 2 : 코너를 4번 돌아야 한다.
-    23: 15 + 2 + 2 + 2 + 2 : 코너를 4번
-
-    => 증가값은 항상 8이다.
-*/
-
+    return new_marbles;
+}
 
 int main(){
-    int N, M, i, j;
+    int N, M, i, j, answer = 0;
     cin >> N >> M;
-    vector<int> marbles (N*N);
+    vector<vector<int>> table(N, vector<int> (N));
+    vector<int> marbles = {};
+
+    for(i=0; i<N; ++i)
+        for(j=0; j<N; ++j)
+            cin >> table[i][j];
+
+    // for(auto a: table){
+    //     for(int b: a)
+    //         cout << b << ' ';
+    //     cout << endl;
+    // }
+
+    // 달팽이 모양을 1자로 펴서 저장하기
+    int current_length = 0, side_length = 1, y = int(N/2), x = int(N/2), turn = 0;
+    vector<vector<int>> directions = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}}; // 좌하우상
+    int i_dir = 0;
+
+    for(i=0; i<N*N; ++i){
+        ++current_length;
+        marbles.push_back(table[y][x]);
+            
+        y += directions[i_dir][0];
+        x += directions[i_dir][1];
+
+        if(current_length % side_length == 0){
+            current_length = 0;
+            i_dir = (i_dir+1) % 4;
+            if(++turn%2 == 0) ++side_length;
+        }
+    }
+
+    int initial_increment[] = {7, 3, 1, 5}, marble_index, cur_increment; // 상하좌우
+    int D, S; // 방향, 거리
+    for(i=0; i<M; ++i){
+        cin >> D >> S;
+
+        // 구슬 파괴
+        cur_increment = initial_increment[D-1];
+        marble_index = initial_increment[D-1];
+        for(j=0; j<S; j++){
+            marbles[marble_index] = 0;
+            cur_increment += 8;
+            marble_index += cur_increment;
+        }
+
+        // cout << endl;
+        // for(int a : marbles) cout << a << ' ';
+        // cout << endl;
+
+        구슬당기기(marbles);
+
+        answer += 구슬폭발(marbles);
+
+        marbles = 구슬변화(marbles);
+    }
+
+    cout << answer << endl;
 
     return 0;
-}   
+}
