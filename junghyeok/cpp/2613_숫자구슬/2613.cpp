d9d30136854981701cf1fffc7f20fdb9ad585737
leejunghyeok <wndgur76@naver.com>
2023-09-22 19:46:15 +1000

    9.22

---
+++ junghyeok/cpp/2613_숫자구슬/2613.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * 최소 최대 그룹합을 변수로 이분탐색
+*/
+
+vector<int> beads;
+int N;
+
+int getGroupN(int maximum){
+    int tmpSum = 0;
+    int groupN = 1;
+    for(int i=0; i<N; ++i){
+        tmpSum += beads[i];
+        if(tmpSum > maximum){
+            ++groupN;
+            tmpSum = beads[i];
+        }
+    }
+    return groupN;
+}
+
+int main(){
+    int M, i, maxN=0, sumN=0;
+    cin >> N >> M;
+    beads = vector<int> (N);
+    
+    for(i=0; i<N; ++i){
+        cin >> beads[i];
+        maxN = beads[i]>maxN? beads[i]:maxN;
+        sumN += beads[i];
+    }
+
+    int left=maxN, right=sumN, mid, groupN;
+    while(left < right){
+        mid = (left + right)/2;
+        // cout << mid << endl;
+        // mid가 최소 최대 그룹합으로 하는 M개의 그룹이 가능한지 확인
+        groupN = getGroupN(mid);
+        // cout << "GN: " << groupN << endl;
+        if(groupN <= M){
+            // 만들수있는 그룹 수가 너무 적으면 right을 mid로 (최대 그룹합을 더 줄인다.)
+            // 같아도 줄이는 이유는 그중 최소값을 찾기위해
+            right = mid;
+        } else{
+            // 그룹이 충분히 많다면 left를 mid로 (최대 그룹합을 늘린다.)
+            left = mid+1;
+        }
+    }
+    cout << right << endl;
+
+    // mid를 최대 그룹합으로 하는 그룹을 짓는다.
+    vector<int> groupSizes (M, 0);
+    int tmpSum = 0, groupIdx = 0, maxGroup;
+    for(i=0; i<N; ++i){
+        tmpSum += beads[i];
+        if(tmpSum > right){
+            ++groupIdx;
+            tmpSum = beads[i];
+        }
+        ++groupSizes[groupIdx];
+
+        // 남은 구슬이 남은 그룹 개수이면 나가서 나머지 1로 채움
+        if(N-i-1 == M-groupIdx-1) break;
+    }
+    for(i=groupIdx+1; i<M; ++i){
+        groupSizes[i] = 1;
+    }
+    
+    for(int size: groupSizes){
+        cout << size << ' ';
+    }
+    cout << endl;
+    return 0;
+}
--- junghyeok/cpp/test.cpp
+++ junghyeok/cpp/test.cpp
@@ -3,7 +3,23 @@
 using namespace std;
 
 int main(){
-    int a;
-    cin >> a;
-    cout << a;
+    vector<int> a = {1, 2, 3};
+    vector<int> b = a;
+    b[0] = 0;
+    cout << "A: ";
+    for(int i: a){
+        cout << i << ' ';
+    }
+    cout << endl << "B: ";
+    for(int i: b){
+        cout << i << ' ';
+    }
+    a = b;
+    b[0] = 100;
+    cout << "A: ";
+    for(int i: a){
+        cout << i << ' ';
+    }
+
+    return 0;
 }
